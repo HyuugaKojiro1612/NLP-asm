e1039924a4188e59edcbf390e8e3e46169da8ab4
HyuugaKojiro1612 <minhquannguyenhue@gmail.com>
2023-12-13 09:38:21 +0700

    part 2: fix grammatical relations

--- model/database.py
+++ model/database.py
@@ -15,41 +15,40 @@ class GrammaticalRelation():
         return rel
 
 
-    def get_tour_relation(self, tour_code, city_name):
-        _pred = '(s PRED DECODE)'
-        _lobj = '(s LOBJ (TOUR t))'
-        _from = '(s FROM (TOUR-CODE tc "{}"))'.format(tour_code)
+    def get_tour_relation(self, tour, city_name):
+        _pred = '(s PRED GO)'
+        _lobj = '(s LOBJ (TOUR t "{}"))'.format(tour)
         _to = '(s TO (CITY-NAME cn "{}"))'.format(city_name)
-        return _pred + _lobj + _from + _to
+        return _pred + _lobj + _to
 
 
-    def get_dtime_relation(self, tour_code, city_code, dtime):
+    def get_dtime_relation(self, tour, city_code, dtime):
         _pred = '(s PRED DEPART)'
-        _lobj = '(s LOBJ (TOUR-CODE tc "{}"))'.format(tour_code)
-        _to = '(s TO (CITY-CODE cc "{}"))'.format(city_code)
+        _lobj = '(s LOBJ (TOUR t "{}"))'.format(tour)
+        _from = '(s FROM (CITY-CODE cc "{}"))'.format(city_code)
         _at = '(s AT (DTIME d {}))'.format(dtime)
-        return _pred + _lobj + _to + _at
+        return _pred + _lobj + _from + _at
 
 
-    def get_atime_relation(self, tour_code, city_code, atime):
+    def get_atime_relation(self, tour, city_code, atime):
         _pred = '(s PRED ARRIVE)'
-        _lobj = '(s LOBJ (TOUR-CODE tc "{}"))'.format(tour_code)
+        _lobj = '(s LOBJ (TOUR t "{}"))'.format(tour)
         _to = '(s TO (CITY-CODE cc "{}"))'.format(city_code)
         _at = '(s AT (ATIME a {}))'.format(atime)
         return _pred + _lobj + _to + _at
 
 
-    def get_runtime_relation(self, tour_code, from_city_code, to_city_code, runtime):
+    def get_runtime_relation(self, tour, from_city_code, to_city_code, runtime):
         _pred = '(s PRED TRAVEL)'
-        _lobj = '(s LOBJ (TOUR-CODE tc "{}"))'.format(tour_code)
+        _lobj = '(s LOBJ (TOUR t "{}"))'.format(tour)
         _from = '(s FROM (CITY-CODE ccf "{}"))'.format(from_city_code)
         _to = '(s TO (CITY-CODE cct "{}"))'.format(to_city_code)
         _for = '(s FOR (RUN-TIME r "{}"))'.format(runtime)
         return _pred + _lobj + _from + _to + _for
 
 
-    def get_transport_relation(self, tour_code, transport):
+    def get_transport_relation(self, tour, transport):
         _pred = '(s PRED TRAVEL)'
-        _lobj = '(s LOBJ (TOUR-CODE tc "{}"))'.format(tour_code)
+        _lobj = '(s LOBJ (TOUR t "{}"))'.format(tour)
         _by = '(s BY (TRANSPORT ts "{}"))'.format(transport)
         return _pred + _lobj + _by
--- part2.py
+++ part2.py
@@ -49,5 +49,5 @@ def solve4():
             file.write('\n')
 
 
-solve2()
+solve3()
 # solve3()
